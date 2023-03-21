c1fd1ff061bb0bb7f9b83252560703f845612a00
oreoanytime <111495669+oreoanytime@users.noreply.github.com>
2023-03-21 12:54:52 +0800

    telegram

--- telegram_api/main.py
+++ telegram_api/main.py
@@ -1,41 +1,51 @@
-from telegram.ext import *
+import telebot 
 import Responses as R
 import os
+import requests
 from dotenv.main import load_dotenv
 
 load_dotenv()
 
-print ("Bot started...")
+API_KEY = os.environ['TELEBOT_API_KEY']
 
-def start_command(update, context):
-    update.message.reply_text('Type something random to get started!')
+bot = telebot.TeleBot(API_KEY)
 
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Hey! Hows it going")
 
-def help_command(update, context):
-    update.message.reply_text('If you need help, you should ask for it on Google!')
-
-def handle_message(update, context):
-    text= str(update.message.text).lower()
-    response = R.sample_responses(text)
-
-    update.message.reply_text(response)
-
-def error(update,context):
-    print(f"Update {update} caused error {context.error}")
+@bot.message_handler(commands=['hello'])
+def hello(message):
+    bot.send_message(message.chat.id, "Hello!")
 
 
-def main():
-    updater = Updater(os.environ['TELEBOT_API_KEY'])
-    dp = updater.dispatcher
+#retrieve items user is currently putting up for listing
+@bot.message_handler(commands=['mylisting'])
 
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("start", help_command))
-    
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
+def start(message):
+    bot.send_message(chat_id=message.chat.id, text="What's your username?")
 
-    dp.add_error_handler(error)
+    # Set the next expected message from the user as the 'name' command
+    bot.register_next_step_handler(message, user_info)
 
-    updater.start_polling()
-    updater.idle()
 
-main()
+def user_info(message):
+    # Get the user ID of the person who sent the message
+    user_id = message.text
+
+    # Call the user microservice API to get user data
+    response = requests.get(f'http://user-microservice/api/v1/users/{user_id}')
+
+
+    if response.status_code == 200:
+        # Retrieve user data from API response
+        user_data = response.json()
+
+        # Send user data as a message to the user in Telegram
+        message = f"Name: {user_data['name']}\nEmail: {user_data['email']}\nPhone: {user_data['phone']}"
+        bot.send_message(chat_id=message.chat.id, text=message)
+    else:
+        # Send error message if request failed
+        bot.send_message(chat_id=message.chat.id, text='Unable to retrieve user data')
+
+bot.polling()
