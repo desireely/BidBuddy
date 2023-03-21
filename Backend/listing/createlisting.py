8f4a86822a1dfb381cc8228bcf945165b306b10a
ccjh23 <clovischowjh@gmail.com>
2023-03-21 20:32:00 +0800

    Create createlisting complex microservice

--- Backend/listing/createlisting.py
+++ Backend/listing/createlisting.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 
 listing_URL = "http://127.0.0.1:5000/listing"
-
+user_URL = "http://127.0.0.1:5005/user/
 
 # Add a new listing
-@app.route("/createlisting", methods=['POST'])
-def add_listing():
+@app.route("/createlisting/<string:userid>", methods=['POST'])
+def add_listing(userid):
     
     if request.is_json:
 
@@ -62,25 +62,53 @@ def processListing(listing):
     transaction_end_datetime = listing_result['data']['transaction_end_datetime']
     transaction_status = listing_result['data']['transaction_status']
 
-    # Preparing message to send via AMQP
-    message = json.dumps(
+    # Invoke the user microservice
+    print('\n-----Invoking user microservice-----')
+    # listing_result = requests.request(url = listing_URL, method='POST', json=listing)
+    user_result = invoke_http(user_URL, method='GET')
+    email = user_result['data']['email']
+    teleid = user_result['data']['teleuser']
+
+    # Preparing message to send via AMQP for email
+    message_email = json.dumps(
         {
-            "user_emails": "Testing",
+            "user_emails": [email],
             "subject": f"{listing_name} Posted Successfully!",
             "html_body": "<strong>Hello, this is a test email.</strong>"
         }
     )
 
-    print(message)
+    print(message_email)
+
+    # Preparing message to send via AMQP for tele
+    message_tele = json.dumps(
+        {
+            "user_teles": [teleid],
+            "subject": f"{listing_name} Posted Successfully!",
+            "body": "Hello, this is a test email."
+        }
+    )
+
+    print(message_tele)
+
 
     # AMQP part
     print('\n\n-----Publishing message-----')        
     amqp_setup.channel.basic_publish(
         exchange=amqp_setup.exchangename, 
         routing_key="sendemail", 
-        body=message, 
+        body=message_email, 
         properties=pika.BasicProperties(delivery_mode=2)
         )
+    
+    print('\n\n-----Publishing tele-----')        
+    amqp_setup.channel.basic_publish(
+        exchange=amqp_setup.exchangename, 
+        routing_key="sendtele", 
+        body=message_tele, 
+        properties=pika.BasicProperties(delivery_mode=2)
+        )
+    
 
     return listing_result
 
