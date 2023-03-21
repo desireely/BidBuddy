483a70a53e61a25219bb71fd24c035a10b2ea327
ccjh23 <clovischowjh@gmail.com>
2023-03-21 20:35:32 +0800

    Edited amqp_setup routing key

--- Backend/listing/amqp_setup.py
+++ Backend/listing/amqp_setup.py
@@ -34,7 +34,7 @@ channel.queue_declare(queue=queue_name, durable=True)
     # 'durable' makes the queue survive broker restarts
 
 #bind new_listing queue
-channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='sendemail') 
+channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.email') 
     # bind the queue to the exchange via the key
     # any routing_key with two words and ending with '.error' will be matched
 
@@ -44,7 +44,7 @@ channel.queue_declare(queue=queue_name, durable=True)
     # 'durable' makes the queue survive broker restarts
 
 #bind new_listing queue
-channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='sendtele') 
+channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.tele') 
     # bind the queue to the exchange via the key
     # any routing_key with two words and ending with '.error' will be matched
 
