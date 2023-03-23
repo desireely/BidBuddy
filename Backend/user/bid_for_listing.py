5fa68e110288774b4c4365645b9a32b2585d1951
dljyuan <127589180+dljyuan@users.noreply.github.com>
2023-03-23 17:42:28 +0800

    completed complex and fixed user

--- Backend/user/bid_for_listing.py
+++ Backend/user/bid_for_listing.py
@@ -1,36 +1,31 @@
 from flask import Flask, request, jsonify
 import requests
 from invokes import invoke_http
+import json
+import amqp_setup
+import pika
 
 app = Flask(__name__)
 
 
 listing_URL = "http://127.0.0.1:5000/listing"
-bid_URL = "http://127.0.0.1:5000/bid"
+bid_URL = "http://127.0.0.1:5020/bid"
 user_URL = "http://127.0.0.1:5005/user"
-# Subjected to Change
-contact_bidders_URL = "http://127.0.0.1:5010/contactbidders"
-
 
 # Creating / Updating bid for specific listing
-@app.route("/bidforlisting/<string:userid>", method=['POST'])
-def post_bid(userid):
+@app.route("/bidforlisting", methods=['POST'])
+def post_bid():
     if request.is_json:
         try:
             bid_details = request.get_json()
             # Add userid into bid_details
-            bid_details['data']['userid'] = userid
-            headers = request.headers
 
-            result = processUserBid(headers,bid_details)
+            result = processUserBid(bid_details)
             print('\n------------------------')
             print('\nresult: ', result)
             return jsonify(result), result["code"]
 
         except Exception as e:
-            
-            print(e)
-
             return jsonify({
                 "code": 500,
                 "message": "listing.py internal error: " + str(e)
@@ -42,52 +37,148 @@ def post_bid(userid):
         "message": "Invalid JSON input: " + str(request.get_data())
     }), 400
 
-def processUserBid(headers, bid_details):
+def processUserBid(bid_details):
 
     # Invoke the listing microservice
     # To get highest bid details (userid & bid price)
-    listing_id = bid_details['data']['listing_details']
+    listing_id = bid_details['listing_id']
     print('\n-----Invoking listing microservice-----')
-    ### bid microservice do not required to filter highest bid ###
     highest_result = invoke_http(listing_URL+'/'+listing_id, method='GET')
-    high_bid_price = highest_result['highest_current_bid']
-    # Add high_bid_price into bid_details
-    bid_details['data']['highest_price'] = high_bid_price
-    # Use for send notification to previous high bidder
-    high_bid_id = highest_result['highest_current_bidder_userid']
+    listing_name = highest_result['data']['listing_name']
 
-    # Invoke the bid microservice
-    # Posting the bid 
+    # Add high_bid_price into bid_details
+    bid_details['highest_current_bid'] = highest_result['data']['highest_current_bid']
+
+    # # Invoke the bid microservice
+    # # Posting the bid 
     print('\n-----Invoking bid microservice-----')
     bid_result = invoke_http(bid_URL, method='POST', json=bid_details)
 
-    # Check the listing result
+    # Check the bid result
     code = bid_result["code"]
     if code not in range(200, 300):
         return bid_result
+    print(bid_details)
     
     # Invoke the listing microservice
     # Update the new highest bidder details
-    new_high = jsonify({
-        "highest_current_bid" : bid_result['data']['bid_price'],
-        "highest_current_bidder_userid" : bid_result['data']['userid']
-    })
+    # new_high = json.dumps({
+    #     "highest_current_bid" : bid_result['data']['bid_price'],
+    #     "highest_current_bidder_userid" : bid_result['data']['user_id']
+    # })
+    bid_details['highest_current_bid'] = bid_result['data']['bid_price']
+    bid_details['highest_current_bidder_userid'] = bid_result['data']['user_id']
+
+
     print('\n-----Invoking listing microservice-----')
-    listing_result = invoke_http(listing_URL+'/'+listing_id, method='PUT', json=new_high)
+    listing_result = invoke_http(listing_URL+'/'+listing_id, method='PUT', json=bid_details)
 
     # Check the listing result
     code = listing_result["code"]
     if code not in range(200, 300):
         return listing_result
     
-    # Add previous highest bidder id
-    ### Do we still need to send highest current bid price since it is the same as bid price ###
-    bid_result['data']['high_current_bid_userid'] = high_bid_id # Should we name it prev high bidder?
+    #AMQP
+    # Send notification for successful bidding
+    print('\n-----Invoking user microservice-----')
+    user_result = invoke_http(user_URL+'/'+bid_details['highest_current_bidder_userid'], method='GET')
+    email = user_result['data']['email']
+    teleid = user_result['data']['teleuser']
 
-    # Invoke the contact bidders microservice
-    print('\n-----Invoking contact bidders microservice-----')
-    listing_result = invoke_http(contact_bidders_URL, method='POST', json=bid_result)
+    # Preparing message to send via AMQP for email
+    message_email = json.dumps(
+        {
+            "user_emails": [email],
+            "subject": f"{listing_name} Posted Successfully!",
+            "html_body": "<strong>Hello, this is a test email.</strong>"
+        }
+    )
 
+    print(message_email)
+
+    # Preparing message to send via AMQP for tele
+    # message_tele = json.dumps(
+    #     {
+    #         "user_teles": [teleid],
+    #         "subject": f"{listing_name} Posted Successfully!",
+    #         "body": "Hello, this is a test email."
+    #     }
+    # )
+
+    # print(message_tele)
+
+
+    # AMQP part
+    print('\n\n-----Publishing message-----')        
+    amqp_setup.channel.basic_publish(
+        exchange=amqp_setup.exchangename, 
+        routing_key="send.email", 
+        body=message_email, 
+        properties=pika.BasicProperties(delivery_mode=2)
+        )
+    
+    # print('\n\n-----Publishing tele-----')        
+    # amqp_setup.channel.basic_publish(
+    #     exchange=amqp_setup.exchangename, 
+    #     routing_key="sendtele", 
+    #     body=message_tele, 
+    #     properties=pika.BasicProperties(delivery_mode=2)
+    #     )
+    
+    #AMQP
+    # Inform previous bidder of outbid status
+    # Use for send notification to previous high bidder
+    if highest_result['data']['highest_current_bidder_userid'] != None:
+        prev_high_id = highest_result['data']['highest_current_bidder_userid']
+    
+        print('\n-----Invoking user microservice-----')
+        user_result = invoke_http(user_URL+'/'+prev_high_id, method='GET')
+        email = user_result['data']['email']
+        teleid = user_result['data']['teleuser']
+
+        # Preparing message to send via AMQP for email
+        message_email = json.dumps(
+            {
+                "user_emails": [email],
+                "subject": f"{listing_name} Posted Successfully!",
+                "html_body": "<strong>Hello, this is a test email.</strong>"
+            }
+        )
+
+        print(message_email)
+
+        # Preparing message to send via AMQP for tele
+        # message_tele = json.dumps(
+        #     {
+        #         "user_teles": [teleid],
+        #         "subject": f"{listing_name} Posted Successfully!",
+        #         "body": "Hello, this is a test email."
+        #     }
+        # )
+
+        # print(message_tele)
+
+
+        # AMQP part
+        print('\n\n-----Publishing message-----')        
+        amqp_setup.channel.basic_publish(
+            exchange=amqp_setup.exchangename, 
+            routing_key="send.email", 
+            body=message_email, 
+            properties=pika.BasicProperties(delivery_mode=2)
+            )
+        
+        # print('\n\n-----Publishing tele-----')        
+        # amqp_setup.channel.basic_publish(
+        #     exchange=amqp_setup.exchangename, 
+        #     routing_key="sendtele", 
+        #     body=message_tele, 
+        #     properties=pika.BasicProperties(delivery_mode=2)
+        #     )
+
+    
+
+    return bid_result
 
 # Run script
 if __name__ == "__main__":
--- Backend/user/show_details_of_bids.py
+++ Backend/user/show_details_of_bids.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 listing_URL = "http://127.0.0.1:5000/listing"
-bid_URL = "http://127.0.0.1:5000/bid"
+bid_URL = "http://127.0.0.1:5020/bid"
 user_URL = "http://127.0.0.1:5005/user"
 
 
@@ -15,9 +15,8 @@ user_URL = "http://127.0.0.1:5005/user"
 def get_user_listing(userid):
     try:
         buyerid = userid
-        headers = request.headers
 
-        result = processUserListing(headers,buyerid)
+        result = processUserListing(buyerid)
         print('\n------------------------')
         print('\nresult: ', result)
         return jsonify(result), result["code"]
@@ -31,19 +30,25 @@ def get_user_listing(userid):
             "message": "listing.py internal error: " + str(e)
         }), 500
 
-def processUserListing(headers, buyerid):
+def processUserListing(buyerid):
 
     # Invoke the bid microservice
     ### Required to return unique listing with user highest bid ###
     print('\n-----Invoking bid microservice-----')
-    bid_result = invoke_http(bid_URL+'/'+buyerid, method='GET')
+    bid_result = invoke_http(bid_URL+'/highest/'+buyerid, method='GET')
 
     # To store user open listing, bid, user details
     open_list = []
     bid_loop = bid_result['data']['bids']
 
+    # Check the listing result
+    code = bid_result["code"]
+    if code not in range(200, 300):
+        return bid_result
+
     for bid in bid_loop:
         # Invoke the listing microservice
+        ### Only display ongoing listing ###
         print('\n-----Invoking listing microservice-----')
         listingid = bid['listing_id']
         listing_result = invoke_http(listing_URL+'/'+listingid, method='GET')
@@ -52,37 +57,38 @@ def processUserListing(headers, buyerid):
         code = listing_result["code"]
         if code not in range(200, 300):
             return listing_result
+    
         
-        # Get seller username
-        sellerid = listing_result['userid']
-        # Invoke the user microservice
-        print('\n-----Invoking user microservice-----')
-        user_result = invoke_http(user_URL+'/'+sellerid, method='GET', headers=headers)
+        if listing_result['data']['status'] == 'open':
+            # Get seller username
+            sellerid = listing_result['data']['userid']
+            # Invoke the user microservice
+            print('\n-----Invoking user microservice-----')
+            user_result = invoke_http(user_URL+'/'+sellerid, method='GET')
 
-        # Check the user result
-        code = user_result["code"]
-        if code not in range(200, 300):
-            return user_result
-        
-        username = user_result['username']
-        
-        # Get all required field
-        req_obj = {
-            "listing_name" : listing_result['listing_name'],
-            "auction_end_date_time" : listing_result['auction_end_date_time'],
-            "seller_username" : username,
-            "highest_bid_price" : listing_result['highest_bid_price'],
-            "bid_price" : bid['bid_price']
-        }
-        open_list.append(req_obj)
+            # Check the user result
+            code = user_result["code"]
+            if code not in range(200, 300):
+                return user_result
+            
+            username = user_result['data']['username']
+            
+            # Get all required field
+            req_obj = {
+                "listing_name" : listing_result['data']['listing_name'],
+                "auction_end_date_time" : listing_result['data']['auction_end_datetime'],
+                "seller_username" : username,
+                "highest_bid_price" : listing_result['data']['highest_current_bid'],
+                "bid_price" : bid['bid_price']
+            }
+            open_list.append(req_obj)
+
 
     # Return all user open listing
-    return jsonify(
-            {
-                "code": 200,
-                "data": open_list
-            }
-    ), 200
+    return {
+        "code": 200,
+        "data": open_list
+    }
 
 # Run script
 if __name__ == "__main__":
