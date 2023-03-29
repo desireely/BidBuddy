6a9a72ca5fad9cd4e6f3c2ed43ec4d9acb085372
rui <regine.sioh.2021@scis.smu.edu.sg>
2023-03-29 19:13:11 +0800

    search fix

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -3,6 +3,10 @@
     <h1 class="pb-3">Listings</h1>
     <div class="row d-flex mx-2">
       <ItemCard v-for="listing in this.filteredListings" :listingData="listing" v-if="filteredListings && filteredListings.length > 0"/>
+      <div class="container-fluid d-flex justify-content-center align-items-center"
+        style="height: calc(100vh - 200px); overflow: hidden;" v-else-if="searchInput">
+        <p class="fs-5" style="color: #C6C6C6">No results found.</p>
+      </div>
       <ItemCard v-for="listing in this.listings" :listingData="listing" v-else/>
     </div>
   </div>
