f4eb160509cde2235e551b8bba867f93cb4f8614
Regine <regine.sioh.2021@smu.edu.sg>
2023-04-02 06:30:24 +0800

    improved itemcards

--- Frontend/src/components/ItemCard.vue
+++ Frontend/src/components/ItemCard.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="col-12 col-sm-6 col-md-4 p-2">
-    <div class="card mb-2">
-      <img src="" class="card-img-top">
-      <div class="card-body">
-        <div class="row align-items-center">
-          <div class="col-7">
-            <div class="d-flex align-items-center me-2">
-              <h5 class="card-text text-start text-wrap">{{ listingData.listing_name }}</h5>
+    <router-link :to="{ path: '/listinginfo', query: { listingID: listingData.listingid } }" style="text-decoration: none; color: inherit;">
+      <div class="card mb-2 mycard">
+        <img src="" class="card-img-top">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col-7">
+              <div class="d-flex align-items-center me-2">
+                <h5 class="card-text text-start text-wrap">{{ listingData.listing_name }}</h5>
+              </div>
+            </div>
+            <div class="col-5 text-end" v-if="!mylistings">
+              <router-link :to="{ path: '/listinginfo', query: { listingID: listingData.listingid } }"
+                class="btn btn-outline-dark py-1 px-2">Place Bid</router-link>
             </div>
           </div>
-          <div class="col-5 text-end">
-            <router-link :to="{ path: '/listinginfo', query: { listingID: listingData.listingid } }"
-              class="btn btn-outline-dark py-1 px-2">Place Bid</router-link>
-          </div>
+          <p class="card-text text-start text-wrap mt-2" style="color: #C6C6C6">Auction ends on <br> {{
+            timeConverter(listingData.auction_end_datetime)
+          }}</p>
+          <hr>
+          <p class="card-text m-0">Start Price: ${{ listingData.starting_bid }}</p>
+          <p class="card-text m-0">Current highest bid: ${{ listingData.highest_current_bid }}</p>
         </div>
-        <p class="card-text text-start text-wrap mt-2" style="color: #C6C6C6">Auction ends on <br> {{
-          timeConverter(listingData.auction_end_datetime)
-        }}</p>
-        <hr>
-        <p class="card-text m-0">Start Price: ${{ listingData.starting_bid }}</p>
-        <p class="card-text m-0">Current highest bid: ${{ listingData.highest_current_bid }}</p>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
@@ -30,6 +32,10 @@ export default {
   name: 'ItemCard',
   props: {
     listingData: null,
+    mylistings: {
+      type: Boolean,
+      default: false
+    }
   },
   methods: {
     timeConverter(UNIX_timestamp) {
@@ -61,4 +67,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+  .mycard:hover {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transform: translateY(-5px);
+    transition: all 0.2s linear;
+    cursor: pointer;
+  }
+</style>
--- Frontend/src/views/MyListings.vue
+++ Frontend/src/views/MyListings.vue
@@ -2,14 +2,14 @@
   <div>
     <h1>My Listings</h1>
     <div class="row d-flex mx-2" v-if="filteredListings && filteredListings.length > 0">
-      <ItemCard v-for="listing in this.filteredListings" :listingData="listing" />
+      <ItemCard v-for="listing in this.filteredListings" :listingData="listing" :mylistings="true"/>
     </div>
     <div class="container-fluid d-flex justify-content-center align-items-center"
       style="height: calc(100vh - 200px); overflow: hidden;" v-else-if="searchInput || (!listings || listings.length == 0)">
       <p class="fs-5" style="color: #C6C6C6">You currently have no listings</p>
     </div>
     <div class="row d-flex mx-2" v-else>
-      <ItemCard v-for="listing in this.listings" :listingData="listing" />
+      <ItemCard v-for="listing in this.listings" :listingData="listing" :mylistings="true"/>
     </div>
   </div>
 </template>
